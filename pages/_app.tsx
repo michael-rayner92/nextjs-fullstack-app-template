39ae00cebb8f74fabd16d81e034f7ba4ee73e2e3
michael-rayner92 <39574445+michael-rayner92@users.noreply.github.com>
2022-08-23 16:56:33 +0200

    feat: implement layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { NextPageWithLayout } from './page';
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || (page => page);
+
+  return <>{getLayout(<Component {...pageProps} />)}</>;
 }
 
 export default MyApp;
